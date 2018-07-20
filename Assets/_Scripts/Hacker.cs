bf36bbaa07cfcd98db281f8aaceb44500031b276
Daniel Buchbauer <daniel@just-my-work.de>
2018-07-20 14:26:16 +0200

    feat: extended user interaction. Recognize input in menu

--- Assets/_Scripts/Hacker.cs
+++ Assets/_Scripts/Hacker.cs
@@ -4,30 +4,49 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour
 {
-
+    string welcome = "Welcome, Hacker-b0Y!";
     // Use this for initialization
     void Start()
     {
-        ShowMainMenu();
+        ShowMainMenu(welcome);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    void ShowMainMenu()
+    void ShowMainMenu(string welcomeMessage)
     {
         Terminal.ClearScreen();
-        Terminal.WriteLine("Welcome to the Matrix.");
+        Terminal.WriteLine(welcomeMessage);
         Terminal.WriteLine("We need to increase our resources.");
         Terminal.WriteLine("What is our next mission?");
-        Terminal.WriteLine("");
+        Terminal.WriteLine("---");
         Terminal.WriteLine("Press 1: Food supplies");
         Terminal.WriteLine("Press 2: Medical supplies");
         Terminal.WriteLine("Press 3: Transport supplies");
-        Terminal.WriteLine("");
+        Terminal.WriteLine("---");
         Terminal.WriteLine("Your choice: ");
+    }
+
+    void OnUserInput(string input)
+    {
+
+        if (input == "1")
+        {
+            Terminal.WriteLine("You choose mission 1.");
+        }
+        else if (input == "2")
+        {
+            Terminal.WriteLine("You choose mission 2.");
+        }
+        else if (input == "3")
+        {
+            Terminal.WriteLine("You choose mission 3.");
+        }
+        else if (input == "menu")
+        {
+            ShowMainMenu(welcome + " Again!");
+        }
+        else
+        {
+            Terminal.WriteLine("Choose a valid mission.");
+        }
     }
 }
