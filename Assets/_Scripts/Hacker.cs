af8f1761a23a2c4e02f2bf703716826545c75e54
Daniel Buchbauer <daniel@just-my-work.de>
2018-07-20 13:22:06 +0200

    feat: added basic menu

---
+++ Assets/_Scripts/Hacker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Terminal.WriteLine("Welcome to the Matrix.");
+        Terminal.WriteLine("We need to increase our resources.");
+        Terminal.WriteLine("What is our next mission?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1: Food supplies");
+        Terminal.WriteLine("Press 2: Medical supplies");
+        Terminal.WriteLine("Press 3: Transport supplies");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Your choice: ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
